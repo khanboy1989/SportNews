8971fbd4847a31e963456e162fdecedd0246ce14
Serhan Khan <s.khan.extern@rewe-group.at>
2023-01-27 22:55:58 +0100

    Adjusted scenedelegate to be trigerred

---
+++ SportNews/Screens/Base/BaseViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BaseViewController.swift
+//  SportNews
+//
+//  Created by Serhan Khan on 27.01.23.
+//
+
+import Foundation
+import UIKit
+import Combine
+class BaseViewController: UIViewController, Alertable {
+ 
+    var disposeBag = Set<AnyCancellable>()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureObservers()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        executeRequests()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+    
+    open func executeRequests() {}
+    
+    open func configureObservers() {}
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        disposeBag.forEach{$0.cancel()}
+        disposeBag.removeAll()
+    }
+    
+    open func didReceiveNetworkError(error:String){
+        showAlert(message: error)
+    }
+
+}
+
